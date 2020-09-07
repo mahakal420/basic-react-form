0a9e2f7f94f36e8881e4ae644444f232f0c3756e
Arpit Dubey <eng.arpit420@gmail.com>
2020-09-07 09:54:51 +0530

    Updates

---
+++ precache-manifest.3fd80da7dbd51fbd4e5a55701c9b81f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2a05040df31a5144d1bb2ff2b6749c",
+    "url": "/basic-react-form/index.html"
+  },
+  {
+    "revision": "02f992f377cf7b4d315d",
+    "url": "/basic-react-form/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "fa1897b03e477b3c9268",
+    "url": "/basic-react-form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "02f992f377cf7b4d315d",
+    "url": "/basic-react-form/static/js/2.dff7e559.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/basic-react-form/static/js/2.dff7e559.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa1897b03e477b3c9268",
+    "url": "/basic-react-form/static/js/main.e923fc0f.chunk.js"
+  },
+  {
+    "revision": "cd9f0a4a79b93416e44f",
+    "url": "/basic-react-form/static/js/runtime-main.a8db7fa9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a7badb74cb5596ddaf6c400717b72689.js"
+  "/basic-react-form/precache-manifest.3fd80da7dbd51fbd4e5a55701c9b81f0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/basic-react-form/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
